c814a127e2ec48cac47e39d45c3e636f1fc70464
Hisashi <cuongthinhvu12@gmail.com>
2025-10-14 21:48:12 +0700

    delete cmd

--- Backend/WCPMS/src/main/java/com/sep490/wcpms/dto/UpdateProfileDTO.java
+++ Backend/WCPMS/src/main/java/com/sep490/wcpms/dto/UpdateProfileDTO.java
@@ -5,14 +5,10 @@ import lombok.Data;
 @Data
 public class UpdateProfileDTO {
     // Chỉ chứa các trường người dùng có thể thay đổi
-    //private String fullName;
     private String email;
     private String phone;
     private String address;
     private String district;
     private String province;
 
-    // Thêm các trường khác nếu cần, ví dụ:
-
-    // private String identityNumber;
 }
