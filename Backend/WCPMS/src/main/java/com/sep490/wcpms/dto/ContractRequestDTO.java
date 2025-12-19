ae39d07185cb32a78663e64be1e2f2b62a786aab
Vu Cuong Thinh <92312422+CuongthinhVu@users.noreply.github.com>
2025-12-19 21:04:17 +0700

    fix lỗi vặt + phân trang

--- Backend/WCPMS/src/main/java/com/sep490/wcpms/dto/ContractRequestDTO.java
+++ Backend/WCPMS/src/main/java/com/sep490/wcpms/dto/ContractRequestDTO.java
@@ -2,6 +2,7 @@ package com.sep490.wcpms.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -21,6 +22,9 @@ public class ContractRequestDTO {
     private Integer routeId;
 
     private String fullName;
+    // Validate số điện thoại VN: Bắt đầu bằng 03, 05, 07, 08, 09 và có tổng 10 số
+    @Pattern(regexp = "^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$",
+            message = "Số điện thoại không đúng định dạng Việt Nam")
     private String phone;
     private String address;
 }
